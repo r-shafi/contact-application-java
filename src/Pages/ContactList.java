c075f07b21d14cbe8c83ab88bfe81fded1bc13d3
Shafi Rayhan <rayhanshafi7@gmail.com>
2024-07-13 20:55:15 +0600

    display contacts, delete contacts

--- src/Pages/ContactList.java
+++ src/Pages/ContactList.java
@@ -1,9 +1,15 @@
 package Pages;
 
-import java.awt.GridLayout;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class ContactList {
@@ -13,19 +19,67 @@ public class ContactList {
     frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     frame.setVisible(true);
 
+    List<String> lines = null;
+
+    try {
+      lines = Files.readAllLines(Paths.get("contacts.txt"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
     JPanel panel = new JPanel();
-    panel.setLayout(new GridLayout(2, 1));
 
-    JLabel label = new JLabel("Todo: Display Contact List");
-    panel.add(label);
+    if (lines.isEmpty()) {
+      JLabel label = new JLabel("No contacts found.");
+      JButton backButton = new JButton("Back");
 
-    JButton backButton = new JButton("Back");
-    panel.add(backButton);
+      panel.add(label);
+      panel.add(backButton);
 
+      backButton.addActionListener(e -> back(frame));
+    } else {
+      for (String line : lines) {
+        String[] parts = line.split(",");
+
+        JPanel contactPanel = new JPanel();
+        contactPanel.setBorder(BorderFactory.createTitledBorder(parts[0]));
+        
+        JLabel phoneLabel = new JLabel(parts[1]);
+        JLabel emailLabel = new JLabel(parts[2]);
+        JButton deleteButton = new JButton("Delete");
+        
+        contactPanel.add(phoneLabel);
+        contactPanel.add(emailLabel);
+        contactPanel.add(deleteButton);
+
+        deleteButton.addActionListener(e -> deleteContact(frame, parts[1]));
+
+        panel.add(contactPanel);
+      }
+    }
     frame.add(panel);
-
-    backButton.addActionListener(e -> back(frame));
   }
+
+  public void deleteContact(JFrame frame, String number) {
+    try {
+      List<String> lines = Files.readAllLines(Paths.get("contacts.txt"));
+      for (String line : lines) {
+        String[] parts = line.split(",");
+        if (parts[1].equals(number)) {
+          lines.remove(line);
+          break;
+        }
+      }
+      Files.write(Paths.get("contacts.txt"), lines);
+      JOptionPane.showMessageDialog(frame, "Contact deleted successfully.");
+      frame.setVisible(false);
+      frame.dispose();
+      new ContactList();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 
   public void back(JFrame frame) {
     frame.setVisible(false);
